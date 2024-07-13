e0274f9e9f8dcd82d83ede9e118c9371f5ba9749
ANGELO-SAGUM <113665565+ANGELO-SAGUM@users.noreply.github.com>
2024-07-13 16:45:49 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const initBalance = ethers.utils.parseEther("1.0");
   const Assessment = await hre.ethers.getContractFactory("Assessment");
-  const assessment = await Assessment.deploy(initBalance);
+  const assessment = await Assessment.deploy({ value: initBalance });
   await assessment.deployed();
   console.log(`A contract with balance of ${ethers.utils.formatEther(initBalance)} ETH deployed to ${assessment.address}`);
 }
