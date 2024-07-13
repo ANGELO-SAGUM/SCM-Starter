02c23a38dcc9a909ff2ebdab82b329329b68d26d
ANGELO-SAGUM <113665565+ANGELO-SAGUM@users.noreply.github.com>
2024-07-13 16:45:11 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -7,10 +7,11 @@ contract Assessment {
 
     event Deposit(address indexed depositor, uint256 amount);
     event Withdraw(address indexed withdrawer, uint256 amount);
+    event Transfer(address indexed sender, address indexed recipient);
 
-    constructor(uint256 initBalance) payable {
+    constructor() payable {
         owner = payable(msg.sender);
-        balance = initBalance;
+        balance = msg.value;
     }
 
     modifier onlyOwner() {
@@ -22,22 +23,39 @@ contract Assessment {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable onlyOwner {
-        uint256 _previousBalance = balance;
-        balance += _amount;
-        assert(balance == _previousBalance + _amount);
-        emit Deposit(msg.sender, _amount);
+    function deposit() public payable onlyOwner {
+        balance += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public onlyOwner {
-        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance(balance, _withdrawAmount);
         }
         balance -= _withdrawAmount;
-        assert(balance == (_previousBalance - _withdrawAmount));
+        payable(msg.sender).transfer(_withdrawAmount);
         emit Withdraw(msg.sender, _withdrawAmount);
+    }
+
+    function depositCustom(uint256 _amount) public payable onlyOwner {
+        balance += _amount;
+        emit Deposit(msg.sender, _amount);
+    }
+
+    function withdrawCustom(uint256 _withdrawAmount) public onlyOwner {
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance(balance, _withdrawAmount);
+        }
+        balance -= _withdrawAmount;
+        payable(msg.sender).transfer(_withdrawAmount);
+        emit Withdraw(msg.sender, _withdrawAmount);
+    }
+
+    function transferAccount(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0), "Invalid new owner address");
+        emit Transfer(msg.sender, _newOwner);
+        owner = payable(_newOwner);
     }
 }
